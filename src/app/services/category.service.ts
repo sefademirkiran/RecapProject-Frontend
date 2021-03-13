e53daa96c9714b43a0d4e91b3b2f8cf2d5367f52
sefademirkiran <64711131+sefademirkiran@users.noreply.github.com>
2021-03-13 22:47:55 +0300

    .

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
-import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoryService {
 
-  apiUrl = 'https://localhost:44386/api/products/getall';
+  apiUrl = 'https://localhost:44386/api/categories/getall';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts() :Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories() :Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
  }
 }
